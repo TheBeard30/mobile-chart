399a4c7b6c4727cf8f6a3039989daa4dc4e0f786
李豪珣 <519367854@qq.com>
2021-09-11 23:57:09 +0800

    添加线图的demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineChartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -0,0 +1,69 @@
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart } from '@antv/g2';
+import { chartData } from './mock.data';
+
+@Component({
+  selector: 'app-line-chart',
+  templateUrl: './line-chart.component.html',
+  styleUrls: ['./line-chart.component.less']
+})
+export class LineChartComponent implements OnInit,AfterViewInit {
+
+  @ViewChild("container") container!: ElementRef;
+
+  chart!: Chart;
+
+  constructor() { }
+  
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const element: HTMLElement = this.container.nativeElement;
+    const domRect = element.getBoundingClientRect();
+    this.chart = new Chart({
+      container: element,
+      width: domRect.width,
+      height: domRect.height
+    });
+
+    this.chart.data(chartData);
+    this.chart.scale({
+      month: {
+        range: [0, 1],
+      },
+      temperature: {
+        nice: true,
+      },
+    });
+
+    this.chart.tooltip({
+      showCrosshairs: true,
+      shared: true,
+    });
+
+    this.chart.axis('temperature', {
+      label: {
+        formatter: (val) => {
+          return val + ' °C';
+        },
+      },
+    });
+
+    this.chart
+      .line()
+      .position('month*temperature')
+      .color('city')
+      .shape('smooth');
+
+    this.chart
+      .point()
+      .position('month*temperature')
+      .color('city')
+      .shape('circle');
+
+    this.chart.render();
+  }
+
+}
