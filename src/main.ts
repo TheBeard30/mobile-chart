86baaccb918aefbca0f6f30ad311c1431d996ce0
李豪珣 <519367854@qq.com>
2021-09-12 22:53:23 +0800

    添加手势操作

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { registerComponentController } from '@antv/g2';
+import Gestrue from '@antv/g2/lib/chart/controller/gesture';
+
 if (environment.production) {
   enableProdMode();
 }
+
+
+
+registerComponentController('gesture', Gestrue);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
