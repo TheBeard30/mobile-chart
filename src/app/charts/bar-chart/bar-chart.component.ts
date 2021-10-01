c8b8d9c29e989474ec481472265aff0e66737e04
李豪珣 <519367854@qq.com>
2021-10-01 21:57:56 +0800

    添加自定义shape

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,18 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Chart } from '@antv/g2';
+import { registerTriangleShape } from 'src/app/shape/interval/triangle.shape';
 
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.less']
 })
-export class BarChartComponent implements OnInit {
+export class BarChartComponent implements OnInit,AfterViewInit {
 
   chart: Chart;
+
+  @ViewChild("container")  container: ElementRef; 
 
   constructor() { }
 
   ngOnInit(): void {
+    registerTriangleShape();
+  }
+
+  ngAfterViewInit(): void{
+    const data = [
+      { type: '未知', value: 654, percent: 0.02 },
+      { type: '17 岁以下', value: 654, percent: 0.02 },
+      { type: '18-24 岁', value: 4400, percent: 0.2 },
+      { type: '25-29 岁', value: 5300, percent: 0.24 },
+      { type: '30-39 岁', value: 6200, percent: 0.28 },
+      { type: '40-49 岁', value: 3300, percent: 0.14 },
+      { type: '50 岁以上', value: 1500, percent: 0.06 },
+    ];
+
+    const element = this.container.nativeElement;
+    const rect = element.getBoundingClientRect();
+
+    this.chart = new Chart({
+      container: element,
+      autoFit: true
+    });
+
+    this.chart.data(data);
+    this.chart.interval().position('type*value').shape('triangle');
+    this.chart.interaction('element-active');
+    this.chart.render();
   }
 
 }
---
+++ src/app/shape/interval/triangle.shape.ts
@@ -0,0 +1,66 @@
+import { registerShape } from '@antv/g2';
+
+registerShape('interval', 'triangle', {
+  // 1. 定义关键点
+  getPoints(cfg) {
+    const x: number = cfg.x as any;
+    const y = cfg.y;
+    const y0 = cfg.y0;
+    const width = cfg.size;
+    return [
+      { x: x - width / 2, y: y0 },
+      { x: x, y: y },
+      { x: x + width / 2, y: y0 },
+    ] as any;
+  },
+  // 2. 绘制
+  draw(cfg, group) {
+    const points = this.parsePoints(cfg.points); // 将0-1空间的坐标转换为画布坐标
+    console.log(cfg);
+    const polygon = group.addShape('path', {
+      attrs: {
+        path: [
+          ['M', points[0].x, points[0].y],
+          ['L', points[1].x, points[1].y],
+          ['L', points[2].x, points[2].y],
+        ],
+        ...cfg.defaultStyle,
+      },
+    });
+    return polygon;
+  },
+});
+
+
+export function registerTriangleShape(){
+    registerShape('interval', 'triangle', {
+        // 1. 定义关键点
+        getPoints(cfg) {
+          const x: number = cfg.x as any;
+          const y = cfg.y;
+          const y0 = cfg.y0;
+          const width = cfg.size;
+          return [
+            { x: x - width / 2, y: y0 },
+            { x: x, y: y },
+            { x: x + width / 2, y: y0 },
+          ] as any;
+        },
+        // 2. 绘制
+        draw(cfg, group) {
+          const points = this.parsePoints(cfg.points); // 将0-1空间的坐标转换为画布坐标
+          console.log(cfg);
+          const polygon = group.addShape('path', {
+            attrs: {
+              path: [
+                ['M', points[0].x, points[0].y],
+                ['L', points[1].x, points[1].y],
+                ['L', points[2].x, points[2].y],
+              ],
+              ...cfg.defaultStyle,
+            },
+          });
+          return polygon;
+        },
+    });
+}
