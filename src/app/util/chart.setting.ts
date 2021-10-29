774e0fb78e6f1675b45ab2de161038503eee9583
李豪珣 <519367854@qq.com>
2021-10-29 23:26:36 +0800

    添加坐标系配置

--- src/app/util/chart.setting.ts
+++ src/app/util/chart.setting.ts
@@ -1,4 +1,6 @@
 import { Chart, Geometry } from "@antv/g2";
+import CoordinateController from "@antv/g2/lib/chart/controller/coordinate";
+import { CoordinateOption } from "@antv/g2/lib/interface";
 
 function pointGeometrySetting(chart: Chart,options: Record<string,any>,xField: string, yField: string,colorField: string,shapeType: string): Geometry{
     return chart.point(options).position(`${xField}*${yField}`).color(colorField).shape(shapeType);
@@ -9,5 +11,10 @@ function lineGeometrySetting(chart: Chart,options: Record<string,any>,xField: st
 }
 
 
-export {pointGeometrySetting,lineGeometrySetting};
+function coordinate(chart: Chart,option: CoordinateOption): CoordinateController{
+    return chart.coordinate(option);
+}
+
+
+export {pointGeometrySetting,lineGeometrySetting,coordinate};
 
